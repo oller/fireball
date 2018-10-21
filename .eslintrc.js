1a4e95de0c14c9303e2bbf49041317550e8348a6
David Ollerhead <david@davidollerhead.com>
2018-10-21 12:51:09 +0100

    Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "node": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "plugin:prettier/recommended",
-        "@vue/prettier"
-    ],
-    "rules": {
-        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    },
-    "parserOptions": {
-        "parser": "babel-eslint"
-    }
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
 }
