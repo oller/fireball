444b5e778e96995fa9bb0ff2ef86e8fbe35de3ce
David Ollerhead <david@davidollerhead.com>
2021-06-27 10:41:13 +0100

    Add more copy to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,13 +6,19 @@
           <h1 class="title">About fireballs</h1>
         </div>
         <div class="column">
+          <p class="m-b-md">
+            Fireballs and bolides are astronomical terms for exceptionally
+            bright meteors that are spectacular enough to to be seen over a very
+            wide area
+          </p>
           <p>
             A <a href="http://vuejs.org">Vue.js</a> case study leveraging NASA's
             <a
               href="https://ssd-api.jpl.nasa.gov/doc/fireball.html"
               target="new"
-              >Fireball Data API</a
             >
+              Fireball Data API
+            </a>
             and Uber's <a href="http://deck.gl/" target="new">Deck.gl</a> to
             interrogate and display fireball and bolide events from around the
             earth.
